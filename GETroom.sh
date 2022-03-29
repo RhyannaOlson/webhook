c37e3b730c3269e13b1b4a056bbb93c172ac8896
rhyannaolson <s3738730@students.fscj.edu>
2022-03-29 08:31:19 -0700

    added script for getting list of rooms

---
+++ GETroom.sh
@@ -0,0 +1,23 @@
+import requests
+import json
+from pprint import pprint
+from getpass import getpass
+
+TOKEN = getpass("Please enter your token: ")
+
+baseURL = "https://webexapis.com"
+room = "/v1/rooms"
+
+body = {}
+
+bodyJSON = json.dumps(body)
+
+headers = {
+    'Authorization': 'Bearer ' + TOKEN,
+    'Content-Type': 'application/json'
+}
+
+createROOM = baseURL + room
+request = requests.get(createROOM, headers=headers, data=bodyJSON)
+response = request.json()
+pprint(response)
